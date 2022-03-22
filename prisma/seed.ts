af5a72112653e8f30db2370336372156acc3a0d8
Andrew Morrow <beardedmongo@gmail.com>
2022-03-22 14:35:39 -0500

    implements seeder for prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import seedData from "../src/pokemonData.json";
+
+const formattedData = seedData.map((pokemon) => {
+  return {
+    id: pokemon.id,
+    name: pokemon.name.english,
+    japanese_name: pokemon.name.japanese,
+    chinese_name: pokemon.name.chinese,
+    french_name: pokemon.name.french,
+    // types: pokemon.type,
+    hp: pokemon.base.hp,
+    attack: pokemon.base.attack,
+    defense: pokemon.base.defense,
+    special_attack: pokemon.base.special_attack,
+    special_defense: pokemon.base.special_defense,
+    speed: pokemon.base.speed,
+  };
+});
+
+async function seeder() {
+  // const createManyTypes = await prisma.type.createMany({
+  //   data: [
+  //     { type: "Grass" },
+  //     { type: "Poison" },
+  //     { type: "Fire" },
+  //     { type: "Flying" },
+  //     { type: "Water" },
+  //     { type: "Bug" },
+  //     { type: "Normal" },
+  //     { type: "Electric" },
+  //     { type: "Ground" },
+  //     { type: "Fairy" },
+  //     { type: "Fighting" },
+  //     { type: "Psychic" },
+  //     { type: "Rock" },
+  //     { type: "Steel" },
+  //     { type: "Ice" },
+  //     { type: "Ghost" },
+  //     { type: "Dragon" },
+  //     { type: "Dark" },
+  //   ],
+  //   skipDuplicates: true,
+  // });
+
+  const createMany = await prisma.pokemon.createMany({
+    data: formattedData,
+    skipDuplicates: true, // Skip 'Bobo'
+  });
+}
+
+seeder()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
