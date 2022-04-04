8848ab908de9f0ef4cf49af32f5a1ceeb05f57ad
Andrew Morrow <beardedmongo@gmail.com>
2022-04-04 13:16:11 -0500

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,8 +14,15 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
     }),
     // ...add more providers here
   ],
